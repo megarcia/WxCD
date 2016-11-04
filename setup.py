9e0c08aea9854b2aebf7640bff414c70529fc403
Matthew Garcia <matt.e.garcia@gmail.com>
2016-11-04 15:45:53 -0500

    PEP8 conformance and some clean-up

--- setup.py
+++ setup.py
@@ -1,51 +1,47 @@
 """
-Python script 'setup.py'
+Python script "setup.py"
 by Matthew Garcia, PhD student
 Dept. of Forest and Wildlife Ecology
 University of Wisconsin - Madison
 matt.e.garcia@gmail.com
 
 Copyright (C) 2015-2016 by Matthew Garcia
-
-DISTRIBUTION and USE subject to 'LICENSE_GnuGPLv3.txt' and 'DISCLAIMER.txt' that accompany 
-this file. This file is provided FREE OF COST and WITHOUT the author's WARRANTY or LIABILITY 
-in any manner whatsoever. This file may be redistributed by the USER as long as the above 
-AUTHORSHIP and COPYRIGHT, this statement, and the accompanying LICENSE and DISCLAIMER files 
-remain intact. Treat others as you would be treated. Pay it forward. Valar dohaeris.
-
 Send questions, bug reports, any related requests to matt.e.garcia@gmail.com
+Treat others as you would be treated. Pay it forward. Valar dohaeris.
 
-REFERENCE: If you use this software, please reference the following in your work products:
-               Garcia, M., and P.A. Townsend (in review): "Recent climatological trends and 
-               potential influences on forest phenology around western Lake Superior, USA." 
-               Submitted to J. Geophys. Res. Atmos. on 5 April 2016, revised 20 August 2016.
-           See also 'README.md', 'CITATION.txt', and 'ACKNOWLEDGEMENTS.txt' for more information.
+USAGE: '$ python setup.py'
 
-USAGE: 'python setup.py'
-
-PURPOSE: Verifies sample data, scripts, modules, documents, and auxiliary files.
-         Verifies availability of python dependencies used by the various scripts.
+PURPOSE: Verifies sample data, scripts, modules, documents, auxiliary files.
+         Verifies availability of python dependencies used by various scripts.
          Uncompresses certain large example data files
          Builds directory structure for script output products.
 
-DEPENDENCIES: all software package source dependencies are polled in this routine
-
-RUN TIME: ~45 sec
+DEPENDENCIES: all software package source dependencies are polled here
 """
 
-import os, sys, glob
 
-def message(str):
-    print str
+import os
+import sys
+import glob
+
+
+def message(char_string):
+    """
+    prints a string to the terminal and flushes the buffer
+    """
+    print char_string
     sys.stdout.flush()
-    return    
+    return
 
-txt_files = ['ACKNOWLEDGEMENTS.txt','CITATION.txt','DISCLAIMER.txt','LICENSE_GnuGPLv3.txt']
+
+txt_files = ['ACKNOWLEDGEMENTS.txt', 'CITATION.txt', 'DISCLAIMER.txt',
+             'LICENSE_GnuGPLv3.txt']
 md_files = ['README.md']
-main_dirs = ['data','docs','source','tools']
+main_dirs = ['data', 'docs', 'source', 'tools']
+#
 scripts = []
 nscripts = 16
-for i in range(0,nscripts):
+for i in range(0, nscripts):
     scripts.append('process_NCEI_%s.py' % str(i).zfill(2))
 scripts.append('process_NCEI_02_preprocess.py')
 scripts.append('process_NCEI_02_serial.py')
@@ -53,30 +49,51 @@ scripts.append('process_NCEI_03_checkpointed.py')
 scripts.append('process_NCEI_03_serial_checkpointed.py')
 scripts.append('process_NCEI_04a.py')
 scripts.append('process_NCEI_04b.py')
-modules = ['Date_Convert.py','Interpolation.py','Plots.py','Read_Header_Files.py',
-           'Stats.py','Teleconnections.py','UTM_Geo_Convert.py']
-dependencies = ['os','sys','datetime','glob','numpy','pandas','h5py','pp','gdal',
-                'osgeo.osr','scipy.interpolate','scipy.ndimage','scipy.stats',
-                'matplotlib.pyplot']
-gz_data_files = ['EPA_L4_Ecoregions_WLS_UTM15N.bil.gz','NCEI_WLS_19830101-20151031.csv.gz',
+#
+modules = ['Date_Convert.py', 'Interpolation.py', 'Plots.py',
+           'Read_Header_Files.py', 'Stats.py', 'Teleconnections.py',
+           'UTM_Geo_Convert.py']
+#
+dependencies = ['os', 'sys', 'datetime', 'glob', 'numpy', 'pandas', 'h5py',
+                'pp', 'matplotlib.pyplot', 'gdal', 'osgeo.osr',
+                'scipy.interpolate', 'scipy.ndimage', 'scipy.stats']
+#
+gz_data_files = ['EPA_L4_Ecoregions_WLS_UTM15N.bil.gz',
+                 'NCEI_WLS_19830101-20151031.csv.gz',
                  'NLCD_2011_WLS_UTM15N.bil.gz']
-data_files = ['EPA_L4_Ecoregions_WLS_polygonIDs.txt','EPA_L4_Ecoregions_WLS_UTM15N.bil',
-              'EPA_L4_Ecoregions_WLS_UTM15N.hdr','NCEI_WLS_19830101-20151031.csv', 
-              'NCEP_CPC_AO_indices.csv','NCEP_CPC_ENSO_indices.csv','NCEP_CPC_NAO_indices.csv',
-              'NCEP_CPC_PNA_indices.csv','NLCD_2011_WLS_UTM15N.bil','NLCD_2011_WLS_UTM15N.hdr',
-              'NOAA_ESRL_AMO_indices.csv','NOAA_ESRL_PDO_indices.csv','NSIDC_MIFL_Superior_Ice.csv',
+#
+data_files = ['EPA_L4_Ecoregions_WLS_polygonIDs.txt',
+              'EPA_L4_Ecoregions_WLS_UTM15N.bil',
+              'EPA_L4_Ecoregions_WLS_UTM15N.hdr',
+              'NCEI_WLS_19830101-20151031.csv',
+              'NCEP_CPC_AO_indices.csv',
+              'NCEP_CPC_ENSO_indices.csv',
+              'NCEP_CPC_NAO_indices.csv',
+              'NCEP_CPC_PNA_indices.csv',
+              'NLCD_2011_WLS_UTM15N.bil',
+              'NLCD_2011_WLS_UTM15N.hdr',
+              'NOAA_ESRL_AMO_indices.csv',
+              'NOAA_ESRL_PDO_indices.csv',
+              'NSIDC_MIFL_Superior_Ice.csv',
               'Query_locations_dates_sample.csv']
-doc_files = ['How_to_get_NCEI_GHCND_data.txt','NCEI_GHCND_documentation.pdf']
+#
+doc_files = ['How_to_get_NCEI_GHCND_data.txt',
+             'NCEI_GHCND_documentation.pdf']
+#
 tools = ['query_NCEI_grids.py']
-add_dirs = ['analyses','grids','images']
-analyses_dirs = ['annual_maps','cluster_maps','ecoregion_maps','figures','summary_maps']
+#
+add_dirs = ['analyses', 'grids', 'images']
+#
+analyses_dirs = ['annual_maps', 'cluster_maps', 'ecoregion_maps',
+                 'figures', 'summary_maps']
 message(' ')
 #
 os.system('rm .DS_Store')
 os.system('rm */.DS_Store')
 os.system('rm ._*')
 os.system('rm */._*')
-message('checking for essential auxiliary files that should accompany this software')
+#
+message('checking for auxiliary files that should accompany this software')
 txts_present = glob.glob('*.txt')
 mds_present = glob.glob('*.md')
 absent = 0
@@ -88,25 +105,26 @@ for txt in txt_files:
         absent += 1
 for md in md_files:
     if md in mds_present:
-        message('- found auxiliary file \'%s\' as expected' % txt)
+        message('- found auxiliary file \'%s\' as expected' % md)
     else:
-        message('- auxiliary file \'%s\' is absent' % txt)
+        message('- auxiliary file \'%s\' is absent' % md)
         absent += 1
 if absent > 0:
-    message('- you don\'t need them to run things, but you do need them to understand things')
+    message('- you don\'t need them to run things, but you do need them to \
+            understand things')
     message('- you should probably download this package again from scratch')
     message('- exiting setup procedure')
     sys.exit(1)
 message(' ')
 #
-message('checking for essential top-level directories that should already exist')
-dirs_present = [ d.replace('/', '') for d in glob.glob('*/') ]
+message('checking for top-level directories that should already exist')
+dirs_present = [d.replace('/', '') for d in glob.glob('*/')]
 absent = 0
-for dir in main_dirs:
-    if dir in dirs_present:
-        message('- found main directory \'%s\' as expected' % dir)
+for dirname in main_dirs:
+    if dirname in dirs_present:
+        message('- found main directory \'%s\' as expected' % dirname)
     else:
-        message('- main directory \'%s\' is absent' % dir)
+        message('- main directory \'%s\' is absent' % dirname)
         absent += 1
 if absent > 0:
     message('- you should download this package again from scratch')
@@ -148,7 +166,8 @@ for srcfile in tools:
         message('- script \'%s\' is absent' % srcfile)
         absent += 1
 if absent > 0:
-    message('- if you need these tools, you should download this package again from scratch')
+    message('- if you need these tools, you should download this package \
+            again from scratch')
 message(' ')
 #
 message('checking for essential python package dependencies for this software')
@@ -199,8 +218,10 @@ try:
     import pp
     message('- python dependency \'pp\' is available')
 except ImportError:
-    message('- essential python dependency \'pp\' (ParallelPython) is not available')
-    message('  (it\'s essential for now, until I develop serial versions of several routines)')
+    message('- essential python dependency \'pp\' (ParallelPython) is not \
+            available')
+    message('  (it\'s essential for now, until I develop serial versions of \
+            several routines)')
     err += 1
 try:
     import gdal
@@ -218,7 +239,8 @@ try:
     import scipy.interpolate
     message('- python dependency \'scipy.interpolate\' is available')
 except ImportError:
-    message('- essential python dependency \'scipy.interpolate\' is not available')
+    message('- essential python dependency \'scipy.interpolate\' is not \
+            available')
     err += 1
 try:
     import scipy.ndimage
@@ -236,11 +258,14 @@ try:
     import matplotlib.pyplot
     message('- python dependency \'matplotlib.pyplot\' is available')
 except ImportError:
-    message('- essential python dependency \'matplotlib.pyplot\' is not available')
+    message('- essential python dependency \'matplotlib.pyplot\' is not \
+            available')
     err += 1
 if err > 0:
-    message('- you need to install one or more additional python packages for this software to work')
-    message('- all of these packages are available via Anaconda (\'conda\') and/or PyPI (\'pip\') repositories')
+    message('- you need to install one or more additional python packages for \
+            this software to work')
+    message('- all of these packages are available via Anaconda (\'conda\') \
+            and/or PyPI (\'pip\') repositories')
     message('- exiting setup procedure')
     sys.exit(1)
 message(' ')
@@ -260,10 +285,11 @@ for gz_dfile in gz_data_files:
         message('- compressed example data file \'%s\' is absent' % gz_dfile)
         absent += 1
 if absent > 0:
-    message('- you don\'t need these if you have your own data in the right formats')
+    message('- you don\'t need these if you have your own data in the right \
+            formats')
     message('- if you need the examples, you can find them at on GitHub at')
-    message('      https://github.com/megarcia/GT16_JGRA')
-#    
+    message('      https://github.com/megarcia/WxCD')
+#
 data_present = glob.glob('data/*')
 absent = 0
 for dfile in data_files:
@@ -274,12 +300,14 @@ for dfile in data_files:
         message('- example data file \'%s\' is absent' % dfile)
         absent += 1
 if absent > 0:
-    message('- you don\'t need these if you have your own data in the right formats')
+    message('- you don\'t need these if you have your own data in the right \
+            formats')
     message('- if you need the examples, you can find them at on GitHub at')
-    message('      https://github.com/megarcia/GT16_JGRA')
+    message('      https://github.com/megarcia/WxCD')
 message(' ')
 #
-message('checking for data documentation files that should accompany this software')
+message('checking for data documentation files that should accompany this \
+        software')
 docs_present = glob.glob('docs/*')
 absent = 0
 for dfile in doc_files:
@@ -295,13 +323,14 @@ if absent > 0:
     message('      https://github.com/megarcia/GT16_JGRA')
 message(' ')
 #
-message('creating top-level and sub-directories that will be used for process output')
-for dir in add_dirs:
-    os.system('mkdir %s' % dir)
-    message('- made top-level directory \'%s\' ' % dir)
-for dir in analyses_dirs:
-    os.system('mkdir analyses/%s' % dir)
-    message('- made sub-directory \'analyses/%s\' ' % dir)
+message('creating top-level and sub-directories that will be used for process \
+        output')
+for dirname in add_dirs:
+    os.system('mkdir %s' % dirname)
+    message('- made top-level directory \'%s\' ' % dirname)
+for dirname in analyses_dirs:
+    os.system('mkdir analyses/%s' % dirname)
+    message('- made sub-directory \'analyses/%s\' ' % dirname)
 message(' ')
 #
 message('copying source scripts and modules to top-level directory')
@@ -315,13 +344,18 @@ message(' ')
 #
 message('all set!')
 message(' ')
-message('make sure to read the \'README.md\' file before you get started on the scripts')
+message('make sure to read the \'README.md\' file before you get started on \
+        the scripts')
 message(' ')
-message('if you need help getting your own dataset of GHCND weather observations, there is')
+message('if you need help getting your own dataset of GHCND weather \
+        observations, there is')
 message('    a how-to document in the \'docs\' directory')
 message(' ')
-message('please send questions, bug reports, any other requests to matt.e.garcia@gmail.com')
+message('please send questions, bug reports, any other requests to \
+        matt.e.garcia@gmail.com')
 message('    (and include a helpfully descriptive subject line, if you could)')
-message('or submit them through the Issues tab at the GitHub repository for this package')
+message('or submit them through the Issues tab at the GitHub repository for \
+        this package')
 message(' ')
+#
 sys.exit(0)
