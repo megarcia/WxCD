cc90cb7d2556032bc6bf690ff1ed74fce53fa1d0
Matthew Garcia <matt.e.garcia@gmail.com>
2016-11-06 18:41:50 -0600

    PEP8 conformance and some clean-up

--- source/process_NCEI_04b.py
+++ source/process_NCEI_04b.py
@@ -6,66 +6,65 @@ University of Wisconsin - Madison
 matt.e.garcia@gmail.com
 
 Copyright (C) 2015-2016 by Matthew Garcia
-
-DISTRIBUTION and USE subject to 'LICENSE_GnuGPLv3.txt' and 'DISCLAIMER.txt' that accompany 
-this file. This file is provided FREE OF COST and WITHOUT the author's WARRANTY or LIABILITY 
-in any manner whatsoever. This file may be redistributed by the USER as long as the above 
-AUTHORSHIP and COPYRIGHT, this statement, and the accompanying LICENSE and DISCLAIMER files 
-remain intact. Treat others as you would be treated. Pay it forward. Valar dohaeris.
-
+Licensed Gnu GPL v3; see 'LICENSE_GnuGPLv3.txt' for complete terms
 Send questions, bug reports, any related requests to matt.e.garcia@gmail.com
+See also 'README.md', 'DISCLAIMER.txt', 'CITATION.txt', 'ACKNOWLEDGEMENTS.txt'
+Treat others as you would be treated. Pay it forward. Valar dohaeris.
 
-REFERENCE: If you use this software, please reference the following in your work products:
-               Garcia, M., and P.A. Townsend (in review): "Recent climatological trends and 
-               potential influences on forest phenology around western Lake Superior, USA." 
-               Submitted to J. Geophys. Res. Atmos. on 5 April 2016, revised 20 August 2016.
-           See also 'README.md', 'CITATION.txt', and 'ACKNOWLEDGEMENTS.txt' for more information.
+PURPOSE: Calculating statistics (mean/stdev/trend/p-value over analysis period)
+         for aggregated climatological grids. Numerous variables are addressed.
 
-USAGE: 'python process_NCEI_04b.py 1984 2013 ./analyses'
+DEPENDENCIES: h5py, numpy, scipy.stats
 
-PURPOSE: Calculating statistics (mean/stdev/trend/p-value over analysis period) for aggregated 
-         climatological grids. Numerous variables are addressed.
+USAGE: '$ python process_NCEI_04b.py 1984 2013 ./analyses'
 
-DEPENDENCIES: Some standard libraries/modules are required, as listed in 'import' lines
-              The h5py module is required for handling of HDF5 files, used here for I/O
-
-INPUT: Output file from process_NCEI_04a.py in '.h5' format with aggregated grid datacubes
-       (with the naming convention 'analyses/[YYYY]-[YYYY]_derived_clim_grids.h5')
+INPUT: '.h5' output file from process_NCEI_04a.py
+       (with the naming convention
+        'analyses/[YYYY]-[YYYY]_derived_clim_grids.h5')
 
 OUTPUT: Same '.h5' file with new calculated statistics datacubes
-
-RUN TIME: ~10 mins per variable
 """
 
-import os, sys, datetime
+
+import sys
+import datetime
 import h5py as hdf
 import numpy as np
 from scipy.stats import pearsonr
 
-def message(str):
-    print str
-    sys.stdout.flush()
-    return    
 
-def regress(ny,yvals):
-    xvals = np.arange(0,ny)
+def message(char_string):
+    """
+    prints a string to the terminal and flushes the buffer
+    """
+    print char_string
+    sys.stdout.flush()
+    return
+
+
+def regress(ny, yvals):
+    xvals = np.arange(0, ny)
     coeffs = np.polyfit(xvals, yvals, 1)
     corr, sig = pearsonr(xvals, yvals)
     return coeffs[0], corr, sig
 
-def write_to_file(file,gvar,gdata):
-    if gvar in file.keys():
-        del file[gvar]
-    file.create_dataset(gvar, data=gdata, dtype=np.float32, compression='gzip')
-    message('- saving %s %s' % (gvar, str(gdata.shape)))
+
+def write_to_file(h5file, gvar, gdata):
+    if gvar in h5file.keys():
+        del h5file[gvar]
+    h5file.create_dataset(gvar, data=gdata, dtype=np.float32,
+                          compression='gzip')
+    message('- saved %s %s' % (gvar, str(gdata.shape)))
     return
 
+
 message(' ')
-message('process_NCEI_04b.py started at %s' % datetime.datetime.now().isoformat())
+message('process_NCEI_04b.py started at %s' %
+        datetime.datetime.now().isoformat())
 message(' ')
 #
 if len(sys.argv) < 4:
-    message('input warning: no input grids directory path indicated, using ./analyses')
+    message('input warning: no input directory indicated, using ./analyses')
     path = './analyses'
 else:
     path = sys.argv[3]
@@ -73,50 +72,49 @@ else:
 if len(sys.argv) < 3:
     message('no dates specified, analyzing 1984-2013 period')
     year_begin = 1984
-    year_end = 2013 
+    year_end = 2013
 else:
-    year_begin = int(sys.argv[1])      
-    year_end = int(sys.argv[2])      
+    year_begin = int(sys.argv[1])
+    year_end = int(sys.argv[2])
 #
-file = '%s/%d-%d_derived_clim_grids.h5' % (path, year_begin, year_end)
-message('extracting variable information from %s' % file)
-h5infile = hdf.File(file,'r') 
-varnames = h5infile.keys()
-h5infile.close()
+h5fname = '%s/%d-%d_derived_clim_grids.h5' % (path, year_begin, year_end)
+message('extracting variable information from %s' % h5fname)
+with hdf.File(h5fname, 'r') as h5infile:
+    varnames = h5infile.keys()
 gridvarnames = [var for var in varnames if var[:6] == 'grids_']
 ngridvars = len(gridvarnames)
 message('- found %d collections of climatological grids' % ngridvars)
 message(' ')
 #
-message('calculating statistics grids and writing to %s' % file)
-h5outfile = hdf.File(file,'r+') 
-if 'stats_order' not in h5outfile['meta'].keys():
-    h5outfile.create_dataset('meta/stats_order', data='mean, stdev, min, max, trend, pearson r, p value')
-    message('- 1 metadata item saved')
-h5outfile.close()
+message('calculating statistics grids and writing to %s' % h5fname)
+with hdf.File(h5fname, 'r+') as h5outfile:
+    if 'stats_order' not in h5outfile['meta'].keys():
+        h5outfile.create_dataset('meta/stats_order',
+                                 data='mean, stdev, min, max, trend, \
+                                 pearson r, p value')
+        message('- 1 metadata item saved')
 #
-for i in range(0,ngridvars):
-    gridvarname = gridvarnames[i]
+for i, gridvarname in enumerate(gridvarnames):
     statsvarname = 'stats_%s' % gridvarname[6:]
-    h5infile = hdf.File(file,'r')
-    gridvar = np.copy(h5infile[gridvarname])
-    h5infile.close()
+    with hdf.File(h5fname, 'r') as h5infile:
+        gridvar = np.copy(h5infile[gridvarname])
     message('- read %s %s' % (gridvarname, str(np.shape(gridvar))))
-    nyears,nrows,ncols = np.shape(gridvar)
-    statscube = np.zeros((7,nrows,ncols))
-    statscube[0,:,:] = np.mean(gridvar, axis=0)
-    statscube[1,:,:] = np.std(gridvar, axis=0)
-    statscube[2,:,:] = np.min(gridvar, axis=0)
-    statscube[3,:,:] = np.max(gridvar, axis=0)
-    for j in range(0,nrows):
-        for i in range(0,ncols):
-            statscube[4,j,i], statscube[5,j,i], statscube[6,j,i] = regress(nyears, gridvar[:,j,i])
-    h5outfile = hdf.File(file,'r+')
-    write_to_file(h5outfile,statsvarname,statscube)
-    h5outfile.close()
+    nyears, nrows, ncols = np.shape(gridvar)
+    statscube = np.zeros((7, nrows, ncols))
+    statscube[0, :, :] = np.mean(gridvar, axis=0)
+    statscube[1, :, :] = np.std(gridvar, axis=0)
+    statscube[2, :, :] = np.min(gridvar, axis=0)
+    statscube[3, :, :] = np.max(gridvar, axis=0)
+    for j in range(nrows):
+        for i in range(ncols):
+            statscube[4, j, i], statscube[5, j, i], statscube[6, j, i] = \
+                regress(nyears, gridvar[:, j, i])
+    with hdf.File(h5fname, 'r+') as h5outfile:
+        write_to_file(h5outfile, statsvarname, statscube)
 message(' ')
 #
-message('process_NCEI_04b.py completed at %s' % datetime.datetime.now().isoformat())
+message('process_NCEI_04b.py completed at %s' %
+        datetime.datetime.now().isoformat())
 message(' ')
 sys.exit(0)
 
